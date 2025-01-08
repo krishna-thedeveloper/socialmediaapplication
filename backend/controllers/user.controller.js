c86495e8eeba6cd1cd024cd98e76e68d79417a43
krishna <krishnathetechdeveloper@gmail.com>
2025-01-08 22:17:03 +0530

    user routes and controllers added

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,4 +1,7 @@
 import User from '../models/user.model.js'
+import Notification from '../models/notification.model.js'
+import bcrypt from 'bcryptjs'
+import {v2 as cloudinary} from 'cloudinary'
 
 export const getUserProfile = async (req,res)=>{
     const {username} = req.params
@@ -7,10 +10,17 @@ export const getUserProfile = async (req,res)=>{
         if(!user){
             return res.status(404).json({error:"user not found"})
         }
-        res.status(200).json(user)
+        return res.status(200).json({
+            "username":user.username,
+            "followers":user.followers,
+            "following":user.following,
+            "fullName":user.fullName,
+            "profileImg":user.profileImg,
+            "coverImg":user.coverImg
+        })
     }catch(error){
         console.log(error.message)
-        res.status(500).json({error:"Internal server error"})
+        return res.status(500).json({error:"Internal server error"})
     }
 }
 
@@ -42,6 +52,82 @@ export  const followunfollowuser = async (req,res)=>{
             await  newNotification.save()
             return res.status(200).json({message:"followed successfully !"})
         }
+    }catch(error){
+        console.log(error.message)
+        return res.status(500).json({error:"Internal server error"})
+    }
+}
+
+export const  getSugguetedUsers = async(req,res)=>{
+    try{
+        const userId = req.user._id
+        const usersFollowedByMe = await User.findById(userId).select("following")
+        const users = await User.aggregate([{
+            $match:{
+                _id:{$ne:userId}
+            }},{$sample:{size:10}}
+        ])
+        const filteredUsers = users.filter(user=>!usersFollowedByMe.following.includes(user._id))
+        const sugguestedUsers= filteredUsers.slice(0,4) 
+        sugguestedUsers.forEach(user => user.password=null)
+        return res.status(200).json(sugguestedUsers)
+    }catch(error){
+        console.log(error.message)
+        return res.status(500).json({error:"Internal server error"})
+    }
+}
+
+export const updateUser = async (req,res)=>{
+    const {fullName,username,email,currentPassword,newPassword,bio,link}=req.body
+    let {profileImg,coverImg}=req.body
+    
+    const userId = req.user._id
+    try{
+        let user = await User.findById(userId)
+        if(!user) return res.status(404).json({error:"user not found"})
+        
+        if((!newPassword && currentPassword) || (newPassword && !currentPassword)){
+            return res.status(404).json({error:"provide both new and currenr password"})
+        }
+        if(currentPassword && newPassword){
+            const isMatch = await bcrypt.compare(currentPassword,user.password)
+
+            if(!isMatch){
+                return res.status(404).json({error:"current password is incorrect"})
+            }
+            if(newPassword.length<6) return res.status(404).json({error:"provide password greater than 6 characters"})
+
+            const salt = await bcrypt.genSalt(10)
+            user.password = await bcrypt.hash(newPassword,salt)
+}
+            
+            if(profileImg){
+                if(user.profileImg){
+                    await cloudinary.uploader.destroy(user.profileImg.split('/')).pop().split('.')[0]
+                }
+                const uploadedResponse = await cloudinary.uploader(profileImg)
+                profileImg = uploadedResponse.secure_url
+            }
+            if(coverImg){
+                if(user.coverImg){
+                    await cloudinary.uploader.destroy(user.coverImg.split('/')).pop().split('.')[0]
+                }
+                const uploadedResponse = await cloudinary.uploader(coverImg)
+                coverImg = uploadedResponse.secure_url
+            }
+
+            user.fullName = fullName || user.fullName
+            user.email = email || user.email
+            user.username = username || user.username
+            user.bio = bio || user.bio
+            user.link = link || user.link
+            user.profileImg = profileImg || user.profileImg
+            user.coverImg = coverImg || user.coverImg
+
+            user = await user.save()
+            user.password = null
+            return res.status(200).json(user)
+        
     }catch(error){
         console.log(error.message)
         return res.status(500).json({error:"Internal server error"})
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import { protectRoute } from '../middleware/protectRoute.js';
-import { getUserProfile,followunfollowuser } from '../controllers/user.controller.js';
+import { getUserProfile,getSugguetedUsers,followunfollowuser,updateUser } from '../controllers/user.controller.js';
 const router = express.Router();
 
 router.get('/profile/:username',protectRoute,getUserProfile)
-//router.get('/sugguested',protectRoute,getSugguetedProfiles)
+router.get('/sugguested',protectRoute,getSugguetedUsers)
 router.get('/follow/:id',protectRoute,followunfollowuser)
-//router.get('/update',protectRoute,updateUserProfile)
+router.post('/update',protectRoute,updateUser)
 export default router;
