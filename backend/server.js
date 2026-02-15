f83cc060cd3974517c19570d9f696073ce165673
krishna-thedeveloper <149251852+krishna-thedeveloper@users.noreply.github.com>
2026-02-15 19:50:05 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ cloudinary.config({
 // Middleware
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
